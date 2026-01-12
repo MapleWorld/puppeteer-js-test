978ad30e1dc64719e8e3ff46a4a6e4cfeeb9028a
MapleWorld <lonely550442072@hotmail.com>
2026-01-12 15:40:48 -0800

    g

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ async function scrapeWebsite() {
             args: [
                 '--no-sandbox',
                 '--disable-setuid-sandbox',
-                '--disable-dev-shm-usage',
                 '--disable-blink-features=AutomationControlled'
             ]
         });
